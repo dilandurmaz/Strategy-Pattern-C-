3a27d23440833c2b52787b26813bca08906bf2d5
dilandurmaz <45289986+dilandurmaz@users.noreply.github.com>
2019-01-21 18:40:52 +0300

    Add files via upload

---
+++ Strategy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Siparis
+{
+    class OdemeIslemleri
+    {
+        private IOdeme _odeme;
+        public OdemeIslemleri(IOdeme _odeme)
+        {
+            this._odeme = _odeme;
+
+        }
+        public void OdemeYap()
+        {
+            this._odeme.OdemeYap();
+        }
+    }
+     
+    interface IOdeme
+    {
+       void OdemeYap();
+    }
+   
+    class NakitOdeme : IOdeme
+    {
+       
+
+        public void OdemeYap()
+        {
+            //int a=1;
+            //if (a==1)
+            //{
+            //    MessageBox.Show("Nakit Ödendi!");
+
+            //}
+            MessageBox.Show("Nakit Ödendi!");
+        }
+
+        
+    }
+
+    class KrediOdeme : IOdeme
+    {
+        
+        
+        public void OdemeYap()
+        {
+
+            //int a = 2;
+            //if (a==2)
+            //{
+            //    MessageBox.Show("Kart  Ödendi!");
+            //}
+            MessageBox.Show("Kart  Ödendi!");
+        }
+
+        
+    }
+  
+}
+
+
